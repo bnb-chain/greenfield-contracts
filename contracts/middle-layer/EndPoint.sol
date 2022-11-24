3a4383d428fc38eca3016033fff1a2fd0dca1c24
Ethan <cosinlinker@gmail.com>
2022-11-24 09:54:48 +0800

    feat: finish EndPoint

--- contracts/middle-layer/EndPoint.sol
+++ contracts/middle-layer/EndPoint.sol
@@ -7,20 +7,13 @@ import "../lib/RLPEncode.sol";
 import "../lib/RLPDecode.sol";
 import "../Config.sol";
 
-
-
 interface IApplication {
-    function handleAckPackage(uint8 channelID, bytes middleMsg, bytes calldata appMsg) external;
-    function handleFailAckPackage(uint8 channelID, bytes middleMsg, bytes calldata appMsg) external;
+    function handleAckPackage(uint8 channelID, bytes calldata appMsg) external;
+    function handleFailAckPackage(uint8 channelID, bytes calldata appMsg) external;
 }
 
 interface ICrossChain {
-    function encodeSynMessage(uint8 eventType, uint8 failureHandling, address receiver, uint256 gasLimit, address refundAddress, bytes memory appMsg) external returns (bytes memory synMessage);
     function sendSynPackage(uint8 channelId, bytes calldata msgBytes, uint256 relayFee) external;
-
-    function cachePackage(uint8 channelId, uint256 sequence, byte32 msgHash, address receiver) external;
-    function retryPackage(uint8 channelId,  address receiver, bytes msg) external;
-    function skipPackage(uint8 channelId,  uint256 sequence) external;
 }
 
 contract EndPoint is Config {
@@ -54,10 +47,17 @@ contract EndPoint is Config {
         address appAddress;
         bytes appMsg;
         bool isFailAck;
+        bool isDeleted;
+        string failReason;
     }
 
     modifier onlyCrossChainContract() {
         require(msg.sender == crosschainContract, "only cross chain contract");
+        _;
+    }
+
+    modifier onlyPackageNotDeleted(bytes32 pkgHash) {
+        require(packageMap[pkgHash].appAddress != address(0) && !packageMap[pkgHash].isDeleted, "package already deleted");
         _;
     }
 
@@ -93,56 +93,6 @@ contract EndPoint is Config {
         ICrossChain(CrossChain).sendSynPackage(APP_CHANNELID, msgBytes, toBFSRelayerFee);
     }
 
-    function retryPackage(address _dstAddress) external payable {
-
-    }
-
-    function skipPackage(address _dstAddress, uint256 _sequence) external payable {
-
-    }
-
-    // @notice receive a payload from ack package
-    // @param _appAddress - the application address
-    // @param _gasLimit - the gas limit for external contract execution
-    // @param _payload - verified payload to send to the destination contract
-    function _receiveMessage(address _appAddress, uint256 _gasLimit, address _refundAddress, bytes calldata _appMsg, bytes calldata _middleMsg, uint256 _remainingFee) internal {
-
-
-        // TODO
-        // refund
-    }
-
-    // receive
-    function handleAckPackage(uint8 channelId, uint256 sequence, bytes calldata msgBytes) external onlyCrossChain {
-        // TODO
-
-        // decode msgBytes => bytes middleMsg, bytes calldata appMsg
-        bytes memory middleMsg;
-        bytes memory appMsg;
-
-        // middleMsg => FailureHandleStrategy failureHandle, address _appAddress, uint256 _gasLimit, address _refundAddress, bytes calldata _appMsg, bytes calldata _middleMsg, uint256 _remainingFee
-        FailureHandleStrategy failureHandle;
-        address _appAddress;
-        uint256 _gasLimit;
-        address _refundAddress;
-        bytes memory _appMsg;
-        bytes memory _middleMsg;
-        uint256 _remainingFee;
-
-        _receiveMessage(_appAddress, _gasLimit, _refundAddress, _appMsg, _middleMsg, _remainingFee);
-    }
-
-    function handleFailAckPackage(uint8 channelId, uint256 sequence, bytes calldata msgBytes) external onlyCrossChain {
-        // decode msgBytes => bytes middleMsg, bytes calldata appMsg
-        // appMsg => _srcAddress, _dstAddress, _restFee, _appPayload
-        // IApplication(_dstAddress).handleFailAckPackage(uint8 channelID, bytes middleMsg, bytes calldata appMsg);
-
-        // 1. HandleInOrder
-
-        // 2. Skip
-
-        // 3. Cache
-    }
 
     function handleAckPackage(uint8 channelId, uint64 sequence, bytes calldata msgBytes) external onlyCrossChain {
         RLPDecode.Iterator memory iter = msgBytes.toRLPItem().iterator();
@@ -178,12 +128,53 @@ contract EndPoint is Config {
 
         if (eventType == EVENT_SEND) {
             bytes32 pkgHash = keccak256(abi.encodePacked(channelId, sequence));
-            _handleSendAckPackage(pkgHash, paramIter, status, errCode);
+            _handleSendAckPackage(pkgHash, paramIter);
         } else {
             revert("unknown event type");
         }
     }
 
+    function handleFailAckPackage(uint8 channelId, uint256 sequence, bytes calldata msgBytes) external onlyCrossChain {
+        RLPDecode.Iterator memory iter = msgBytes.toRLPItem().iterator();
+        uint8 eventType = uint8(iter.next().toUint());
+        RLPDecode.Iterator memory paramIter;
+        if (iter.hasNext()) {
+            paramIter = iter.next().toBytes().toRLPItem().iterator();
+        } else {
+            revert("empty fail ack package");
+        }
+        if (eventType == EVENT_SEND) {
+            bytes32 pkgHash = keccak256(abi.encodePacked(channelId, sequence));
+            _handleSendFailAckPackage(pkgHash, paramIter);
+        } else {
+            revert("unknown event type");
+        }
+    }
+
+    function retryPackage(bytes32 pkgHash) external onlyPackageNotDeleted(pkgHash) {
+        address appAddress = msg.sender;
+        require(failureHandleMap[appAddress] != FailureHandleStrategy.Closed, "strategy not allowed");
+
+        if (failureHandleMap[appAddress] == FailureHandleStrategy.HandleInOrder) {
+            require(retryQueue.front() == pkgHash, "package not on front");
+            retryQueue.popFront();
+        }
+
+        if (packageMap[pkgHash].isFailAck) {
+            IApplication(appAddress).handleFailAckPackage(APP_CHANNELID, _appMsg);
+        } else {
+            IApplication(appAddress).handleAckPackage(APP_CHANNELID, _appMsg);
+        }
+        packageMap[pkgHash].isDeleted = true;
+    }
+
+    function skipPackage(bytes32 pkgHash) external onlyPackageNotDeleted(pkgHash) {
+        if (failureHandleMap[appAddress] == FailureHandleStrategy.HandleInOrder) {
+            require(retryQueue.front() == pkgHash, "package not on front");
+            retryQueue.popFront();
+        }
+        packageMap[pkgHash].isDeleted = true;
+    }
 
 
     /***************************** Internal functions *****************************/
@@ -195,7 +186,7 @@ contract EndPoint is Config {
     }
 
     /************************* Handle cross-chain package *************************/
-    function _handleSendAckPackage(bytes32 pkgHash, RLPDecode.Iterator memory paramIter, uint8 status, uint8 errCode) internal {
+    function _handleSendAckPackage(bytes32 pkgHash, RLPDecode.Iterator memory paramIter) internal {
         bool success;
         uint256 idx;
 
@@ -230,10 +221,59 @@ contract EndPoint is Config {
         uint256 gasBefore = gasleft();
         try IApplication(_appAddress).handleAckPackage{ gas: _gasLimit}(APP_CHANNELID, _appMsg) {
         } catch (bytes memory reason) {
-            packageMap[pkgHash] = RetryPackage(_appAddress, _appMsg, false);
-            retryQueue.pushBack(pkgHash);
+            if (_strategy != FailureHandleStrategy.Skip) {
+                packageMap[pkgHash] = RetryPackage(_appAddress, _appMsg, false, reason);
+                retryQueue.pushBack(pkgHash);
+            }
         }
 
+        uint256 gasUsed = gasleft() - gasBefore;
+        uint256 refundFee = _callbackFee - gasUsed * callbackGasprice;
+
+        // refund
+        (bool success,) = _refundAddress.call{ gas: transferGas, value: refundFee }("");
+    }
+
+    function _handleSendFailAckPackage(bytes32 pkgHash, RLPDecode.Iterator memory paramIter) internal {
+        bool success;
+        uint256 idx;
+
+        address _appAddress;
+        address _refundAddress;
+        uint256 _callbackFee;
+        uint256 _gasLimit;
+        FailureHandleStrategy _strategy;
+        bytes memory _appMsg;
+
+        while (paramIter.hasNext()) {
+            if (idx == 0) {
+                _appAddress = address(uint160(paramIter.next().toAddress()));
+            } else if (idx == 1) {
+                _refundAddress = address(uint160(paramIter.next().toAddress()));
+            } else if (idx == 2) {
+                _callbackFee = uint256(paramIter.next().toUint());
+            } else if (idx == 3) {
+                _gasLimit = uint256(paramIter.next().toUint());
+            } else if (idx == 4) {
+                _strategy = uint8(paramIter.next().toUint());
+            } else if (idx == 5) {
+                _appMsg = uint256(paramIter.next().toBytes());
+                success = true;
+            } else {
+                break;
+            }
+            ++idx;
+        }
+        require(success, "rlp decode failed");
+
+        uint256 gasBefore = gasleft();
+        try IApplication(_appAddress).handleFailAckPackage{ gas: _gasLimit}(APP_CHANNELID, _appMsg) {
+        } catch (bytes memory reason) {
+            if (_strategy != FailureHandleStrategy.Skip) {
+                packageMap[pkgHash] = RetryPackage(_appAddress, _appMsg, true, reason);
+                retryQueue.pushBack(pkgHash);
+            }
+        }
         uint256 gasUsed = gasleft() - gasBefore;
         uint256 refundFee = _callbackFee - gasUsed * callbackGasprice;
 
