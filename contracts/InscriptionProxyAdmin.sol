f95da0779ad6a2b8144784335f72c693af4abe58
Ethan <cosinlink@163.com>
2023-01-06 10:21:29 +0800

    feat: remove constructor in ProxyAdmin

--- contracts/InscriptionProxyAdmin.sol
+++ contracts/InscriptionProxyAdmin.sol
@@ -1,8 +1,4 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
-contract InscriptionProxyAdmin is ProxyAdmin {
-    constructor(address _govHubProxy) {
-        _transferOwnership(_govHubProxy);
-    }
-}
+contract InscriptionProxyAdmin is ProxyAdmin {}
