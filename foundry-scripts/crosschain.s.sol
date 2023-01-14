dad843890aae2ade4a70f24d855faa222c2e4d56
Ethan <cosinlinker@gmail.com>
2023-01-14 14:22:53 +0800

    feat: add crosschain script

---
+++ foundry-scripts/crosschain.s.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.8.0;
+import "forge-std/Script.sol";
+
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+import "../contracts/InscriptionProxy.sol";
+import "../contracts/InscriptionProxyAdmin.sol";
+import "../contracts/InscriptionLightClient.sol";
+import "../contracts/CrossChain.sol";
+import "../contracts/middle-layer/GovHub.sol";
+import "../contracts/middle-layer/TokenHub.sol";
+
+contract CrossChainScript is Script {
+    CrossChain private crosschain = CrossChain(address());
+
+    function run(bytes calldata _payload, bytes calldata _blsSignature, uint256 _validatorsBitSet) public {
+        uint256 privateKey = uint256(vm.envBytes32('PK1'));
+        address developer = vm.addr(privateKey);
+
+        vm.startBroadcast();
+
+        crosschain.handlePackage(_payload, _blsSignature, _validatorsBitSet);
+
+        vm.stopBroadcast();
+    }
+
+}
