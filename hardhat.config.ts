43929bb181636b09b763b2378f2c89b2ea6ca6f2
Ethan <cosinlinker@gmail.com>
2023-02-13 12:45:07 +0800

    chores: modify config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,6 +22,13 @@ const config: HardhatUserConfig = {
                 '0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6',
             ]
         },
+        'qa': {
+            url: process.env.BSC_QA || "http://127.0.0.1:8545",
+            accounts: [
+                process.env.DeployerPrivateKey || '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',  // developer
+                process.env.RelayerPrivateKey || '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',  // relayer
+            ]
+        },
         'local': {
             url: process.env.BSC_LOCAL || "http://127.0.0.1:8545",
             accounts: [
