75a6a3b0b7ba7956c6d55de8d91b3b0c5b4510b4
Ethan <cosinlinker@gmail.com>
2023-03-09 07:54:28 +0800

    fix: modify channel id for channels

--- contracts/Config.sol
+++ contracts/Config.sol
@@ -8,12 +8,12 @@ abstract contract Config {
     uint8 public constant TRANSFER_IN_CHANNEL_ID = 0x01;
     uint8 public constant TRANSFER_OUT_CHANNEL_ID = 0x02;
     uint8 public constant GOV_CHANNEL_ID = 0x03;
-    uint8 public constant APP_CHANNEL_ID = 0x05;
+    uint8 public constant APP_CHANNEL_ID = 0x04;
 
     // TODO channel ID
-    uint8 public constant BUCKET_CHANNEL_ID = 0x06;
-    uint8 public constant OBJECT_CHANNEL_ID = 0x07;
-    uint8 public constant GROUP_CHANNEL_ID = 0x08;
+    uint8 public constant BUCKET_CHANNEL_ID = 0x05;
+    uint8 public constant OBJECT_CHANNEL_ID = 0x06;
+    uint8 public constant GROUP_CHANNEL_ID = 0x07;
 
     // contract address
     // will calculate their deployed addresses from deploy script
