c4de3b193d0447b1c1cd14c07b5c491c3c8d8b46
Ethan <cosinlinker@gmail.com>
2023-03-08 15:43:49 +0800

    chores: modify default constant contracts to address(0) before deployed

--- contracts/Config.sol
+++ contracts/Config.sol
@@ -15,12 +15,12 @@ abstract contract Config {
 
     // contract address
     // will calculate their deployed addresses from deploy script
-    address public constant PROXY_ADMIN = 0xB5d064b44960FdedA1072f983C3E8f1e123cE154;
-    address public constant GOV_HUB = 0xA43C8fA0cb6567312091fb14ebf4d0f65De4a6E4;
-    address public constant CROSS_CHAIN = 0x39c3A55F68Bf9f2992776991F25Aac6813a4F1d0;
-    address public constant TOKEN_HUB = 0x5bFD50cBC7139F731a576a8dd7375c8D0ec48eba;
-    address public constant LIGHT_CLIENT = 0x0D077176f54744A78Da0B8CBB58Fdd76552B4ead;
-    address public constant RELAYER_HUB = 0x3fe71142DFD985400eedE943A5494c7310B4af18;
+    address public constant PROXY_ADMIN = address(0);
+    address public constant GOV_HUB = address(0);
+    address public constant CROSS_CHAIN = address(0);
+    address public constant TOKEN_HUB = address(0);
+    address public constant LIGHT_CLIENT = address(0);
+    address public constant RELAYER_HUB = address(0);
 
     modifier onlyGov() {
         require(msg.sender == GOV_HUB, "only GovHub contract");
