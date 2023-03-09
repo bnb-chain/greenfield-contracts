f475d4f9d6a47dc85d6e51f27397b4e2aa1de890
Ethan <cosinlinker@gmail.com>
2023-03-09 19:14:26 +0800

    feat: remove app_channel_id

--- contracts/Config.sol
+++ contracts/Config.sol
@@ -8,7 +8,6 @@ abstract contract Config {
     uint8 public constant TRANSFER_IN_CHANNELID = 0x01;
     uint8 public constant TRANSFER_OUT_CHANNELID = 0x02;
     uint8 public constant GOV_CHANNELID = 0x03;
-    uint8 public constant APP_CHANNELID = 0x04;
 
     uint32 public constant CODE_OK = 0;
     uint32 public constant ERROR_FAIL_DECODE = 100;
