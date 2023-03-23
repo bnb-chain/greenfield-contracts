dd43b3418c3d788f227f378e1a89dd1477951b57
Ethan <cosinlinker@gmail.com>
2023-03-23 16:40:55 +0800

    feat: rebase from develop branch

--- contracts/interface/ILightClient.sol
+++ contracts/interface/ILightClient.sol
@@ -10,11 +10,15 @@ interface ILightClient {
         uint256 end;
     }
 
-    function verifyPackage(bytes calldata _payload, bytes calldata _blsSignature, uint256 _validatorSet)
-        external
-        view
-        returns (bool verified);
+    function verifyPackage(
+        bytes calldata _payload,
+        bytes calldata _blsSignature,
+        uint256 _validatorSet
+    ) external view returns (bool verified);
+
     function getRelayers() external view returns (address[] memory);
+
     function getInturnRelayer() external view returns (InturnRelayer memory);
+
     function getInturnRelayerAddress() external view returns (address);
 }
