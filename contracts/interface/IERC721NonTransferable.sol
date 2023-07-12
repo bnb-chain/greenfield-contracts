a65fefe4c2232f85a52dcd13ee0f3a19f978d0d5
Roshan <48975233+Pythonberg1997@users.noreply.github.com>
2023-07-12 18:15:44 +0800

    chore: refactor `IERC721NonTransferable` and `IERC1155NonTransferable` (#50)

--- contracts/interface/IERC721NonTransferable.sol
+++ contracts/interface/IERC721NonTransferable.sol
@@ -1,42 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+
 pragma solidity ^0.8.0;
 
 /**
  * @dev External interface of ERC721NonTransferable declared to support ERC165 detection.
  *
- * This Non-Fungible Token doesn't support token transfer or approval.
+ * This Non-Fungible Token DOESN'T support token transfer or approval.
  */
-interface IERC721NonTransferable {
-    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
-    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
-
-    function name() external view returns (string memory);
-
-    function symbol() external view returns (string memory);
-
-    function balanceOf(address owner) external view returns (uint256 balance);
-
-    function ownerOf(uint256 tokenId) external view returns (address owner);
-
+interface IERC721NonTransferable is IERC721Enumerable {
     function exists(uint256 tokenId) external view returns (bool);
-
-    function tokenURI(uint256 tokenId) external view returns (string memory);
 
     function setBaseURI(string calldata newURI) external;
 
     function mint(address to, uint256 tokenId) external;
 
     function burn(uint256 tokenId) external;
-
-    function getApproved(uint256 tokenId) external view returns (address operator);
-
-    function isApprovedForAll(address owner, address operator) external view returns (bool);
-
-    function totalSupply() external view returns (uint256);
-
-    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
-
-    function tokenByIndex(uint256 index) external view returns (uint256);
 }
