d83b8a72a8d006d91df1ed44df79e54a5f2e0ffc
Ethan <cosinlinker@gmail.com>
2024-03-13 10:10:33 +0800

    feat: add interface

--- contracts/interface/IMultiMessage.sol
+++ contracts/interface/IMultiMessage.sol
@@ -4,4 +4,10 @@ pragma solidity ^0.8.0;
 
 import "../middle-layer/resource-mirror/storage/MultiStorage.sol";
 
-interface IMultiMessage {}
+interface IMultiMessage {
+    function sendMessages(
+        address[] calldata _targets,
+        bytes[] calldata _data,
+        uint256[] calldata _values
+    ) external payable returns (bool);
+}
