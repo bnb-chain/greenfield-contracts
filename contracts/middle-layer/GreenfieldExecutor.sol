532afad84582991cc9b2d2c4bbe0093f51cf0338
Roshan <luoshen1997@gmail.com>
2024-03-25 16:14:07 +0800

    add annotation

--- contracts/middle-layer/GreenfieldExecutor.sol
+++ contracts/middle-layer/GreenfieldExecutor.sol
@@ -9,27 +9,33 @@ import "../interface/IGreenfieldExecutor.sol";
 import "../interface/IMiddleLayer.sol";
 
 contract GreenfieldExecutor is Config, Initializable, IMiddleLayer, IGreenfieldExecutor {
-    // Supported message types and its corresponding number
-    // 1: CreatePaymentAccount
-    // 2: Deposit
-    // 3: DisableRefund
-    // 4: Withdraw
-    // 5: MigrateBucket
-    // 6: CancelMigrateBucket
-    // 7: UpdateBucketInfo
-    // 8: ToggleSPAsDelegatedAgent
-    // 9: SetBucketFlowRateLimit
-    // 10: CopyObject
-    // 11: UpdateObjectInfo
-    // 12: UpdateGroupExtra
-    // 13: SetTag
-
     constructor() {
         _disableInitializers();
     }
 
     function initialize() public initializer {}
 
+    /**
+     * @notice Send messages to GNFD
+     *
+     * @param _msgTypes The greenfield message types.
+     * Supported message types and its corresponding number
+     * 1: CreatePaymentAccount
+     * 2: Deposit
+     * 3: DisableRefund
+     * 4: Withdraw
+     * 5: MigrateBucket
+     * 6: CancelMigrateBucket
+     * 7: UpdateBucketInfo
+     * 8: ToggleSPAsDelegatedAgent
+     * 9: SetBucketFlowRateLimit
+     * 10: CopyObject
+     * 11: UpdateObjectInfo
+     * 12: UpdateGroupExtra
+     * 13: SetTag
+     * @param _msgBytes The greenfield message bytes encoded by protobuf.
+     * Please refer to https://docs.bnbchain.org/greenfield-docs/docs/guide/core-concept/cross-chain/executor/ for more details.
+     */
     function execute(uint8[] calldata _msgTypes, bytes[] calldata _msgBytes) external payable override returns (bool) {
         uint256 _length = _msgTypes.length;
         require(_length > 0, "empty data");
