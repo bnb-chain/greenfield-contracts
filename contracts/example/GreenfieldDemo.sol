28023d06bd5eff13e4da5e0613c019653c6f1632
Ethan <cosinlinker@gmail.com>
2024-07-01 15:07:37 +0800

    chores: lint code

--- contracts/example/GreenfieldDemo.sol
+++ contracts/example/GreenfieldDemo.sol
@@ -15,19 +15,26 @@ contract GreenfieldDemo {
     address public constant SP_ADDRESS_TESTNET = 0x5FFf5A6c94b182fB965B40C7B9F30199b969eD2f;
     address public constant GREENFIELD_EXECUTOR = 0x3E3180883308e8B4946C9a485F8d91F8b15dC48e;
 
-    function createBucket(string memory bucketName, uint256 transferOutAmount, bytes memory _executorData) external payable {
+    function createBucket(
+        string memory bucketName,
+        uint256 transferOutAmount,
+        bytes memory _executorData
+    ) external payable {
         (uint256 relayFee, uint256 ackRelayFee) = ICrossChain(CROSS_CHAIN).getRelayFees();
         require(msg.value == transferOutAmount + relayFee * 3 + ackRelayFee * 2, "msg.value not enough");
 
         // 1. transferOut to address(this) on greenfield
-        ITokenHub(TOKEN_HUB).transferOut{value: transferOutAmount + relayFee + ackRelayFee}(address(this), transferOutAmount);
+        ITokenHub(TOKEN_HUB).transferOut{ value: transferOutAmount + relayFee + ackRelayFee }(
+            address(this),
+            transferOutAmount
+        );
 
         // 2. set bucket flow rate limit
         uint8[] memory _msgTypes = new uint8[](1);
-        _msgTypes[0] = 9;  // * 9: SetBucketFlowRateLimit
+        _msgTypes[0] = 9; // * 9: SetBucketFlowRateLimit
         bytes[] memory _msgBytes = new bytes[](1);
         _msgBytes[0] = _executorData;
-        IGreenfieldExecutor(GREENFIELD_EXECUTOR).execute{value: relayFee}(_msgTypes, _msgBytes);
+        IGreenfieldExecutor(GREENFIELD_EXECUTOR).execute{ value: relayFee }(_msgTypes, _msgBytes);
 
         // 3. create bucket, owner = address(this)
         BucketStorage.CreateBucketSynPackage memory createPackage = BucketStorage.CreateBucketSynPackage({
